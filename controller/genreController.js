d54cb4695c1e8305adf5638b4b530a42fe298dc7
STARZYYY12 <rizkyfaiz324@gmail.com>
2026-08-10 14:50:57 +0700

    Add getGenreById function to fetch genre by ID

--- controller/genreController.js
+++ controller/genreController.js
@@ -9,3 +9,17 @@ async function getAllGenre(req, res) {
     res.status(500).json({ error: "failed to fetch genre" });
   }
 }
+
+async function getGenreById(req, res) {
+  const { id } = req.params;
+  try {
+    const genre = await db.Genre.findByPk(id);
+    if (!genre) {
+      return res.status(404).json({ error: "genre tidak ditemukan" });
+    }
+    res.status(200).json(genre);
+  } catch (err) {
+    console.error("error fetching genre by id: ", err.message);
+    res.status(500).json({ error: "failed to fetch genre by id" });
+  }
+}
