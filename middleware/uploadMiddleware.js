1a84e18bc3a0324dfbab5423045dd5c3d8a2add3
STARZYYY12 <rizkyfaiz324@gmail.com>
2026-08-10 14:47:47 +0700

    Add multer filename configuration

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -5,3 +5,9 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
+
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
